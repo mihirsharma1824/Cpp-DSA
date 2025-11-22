e235e9aca7486dcd79a63d2b0ce024ae80576c65
mihirsharma1824 <mihirsharma1824@gmail.com>
2025-11-22 23:50:18 +0530

    patterns

--- day1-patterns.cpp
+++ day1-patterns.cpp
@@ -192,15 +192,11 @@ for(int j=0;j<i*2-1;j++){
 cout<<endl;
 }
 //lower traingle 
-
 //space 1
 for(int i=0;i<n-1;i++){
 for(int j=0;j<i+1;j++){
     cout<<" ";
 }cout<<"*";
-
-
-
 //space 2
 if(i!= n-2){ 
         for(int j=0;j<4-2*i-1;j++){
@@ -210,6 +206,5 @@ if(i!= n-2){
 }
 cout<<endl;
 }
-
 return 0;
 }
